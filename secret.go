a1d9a8c6135c059e03fc7c2fa4e22ccdc658c7cc
huykingsofm <huykingsofm@gmail.com>
2024-10-20 07:40:59 +0700

    Update config for authorization code flow

--- secret.go
+++ secret.go
@@ -3,7 +3,9 @@ package config
 type Secret struct {
 	Postgres       PostgresSecret       `envconfig:"postgres"`
 	Authentication AuthenticationSecret `envconfig:"auth"`
+	OAuth2         OAuth2Secret         `envconfig:"oauth2"`
 	Redis          RedisSecret          `envconfig:"redis"`
+	Session        SessionSecret        `envconfig:"session"`
 }
 
 type PostgresSecret struct {
@@ -22,7 +24,16 @@ type AuthenticationSecret struct {
 	TokenHMACSecretKey string `envconfig:"token_hmac_secret_key"`
 }
 
+type OAuth2Secret struct {
+	IdPSecret string `envconfig:"idp_secret"`
+}
+
 type RedisSecret struct {
 	Username string `envconfig:"username"`
 	Password string `envconfig:"password"`
+}
+
+type SessionSecret struct {
+	AuthenticationKey string `envconfig:"authentication_key"`
+	EncryptionKey     string `envconfig:"encryption_key"`
 }
