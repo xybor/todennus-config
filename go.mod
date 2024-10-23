f79034ee32d068958ceea460fd8f13a0c5762943
huykingsofm <huykingsofm@gmail.com>
2024-10-23 11:20:05 +0700

    add config for consent page

--- go.mod
+++ go.mod
@@ -7,6 +7,6 @@ toolchain go1.23.2
 require (
 	github.com/joho/godotenv v1.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/xybor/x v1.9.0
+	github.com/xybor/x v1.10.0
 	gorm.io/gorm v1.25.12
 )
--- variable.go
+++ variable.go
@@ -85,20 +85,47 @@ func DefaultAuthenticationVariable() AuthenticationVariable {
 }
 
 type OAuth2Variable struct {
-	IdPLoginURL                      string `envconfig:"idp_login_url"`
-	ClientSecretLength               int    `envconfig:"client_secret_length"`
-	AuthorizationCodeFlowExpiration  int    `envconfig:"authorization_code_flow_expiration"` // in second
-	AuthenticationCallbackExpiration int    `envconfig:"authentication_callback_expiration"` // in second
-	SessionUpdateExpiration          int    `envconfig:"session_update_expiration"`          // in second
+	IdPLoginURL        string `envconfig:"idp_login_url"`
+	ClientSecretLength int    `envconfig:"client_secret_length"`
+
+	// AuthorizationCodeFlowExpiration is the timeout which the code must be
+	// exchanged.
+	AuthorizationCodeFlowExpiration int `envconfig:"authorization_code_flow_expiration"` // in second
+
+	// AuthenticationCallbackExpiration is the timeout which the authorization
+	// flow is waiting for the authentication result. Within this time, the IdP
+	// must send the result to /auth/callback. Otherwise, user must go back to
+	// the Client App to authenticate again.
+	AuthenticationCallbackExpiration int `envconfig:"authentication_callback_expiration"` // in second
+
+	// SessionUpdateExpiration is the timeout which the authentication result
+	// is stored. Within this time, user must be redirected to /session/update
+	// to update the session. Otherwise, user may be redirected to the login
+	// page again.
+	SessionUpdateExpiration int `envconfig:"session_update_expiration"` // in second
+
+	// FailureConsentExpiration is the timeout which consent failure result is
+	// temporarily stored. Within this time, user must be redirected to
+	// /oauth2/authorize to responds to Client about the failure result.
+	// Otherwise, user will be redirected to consent page again.
+	FailureConsentExpiration int `envconfig:"consent_session_expiration"` // in second
+
+	// ConsentExpiration is the timeout which the consent success result is
+	// stored. Within this time, every request to /oauth2/authorize will be
+	// automatically accepted by user. After this time, user will be redirected
+	// to consent page again.
+	ConsentExpiration int `envconfig:"consent_expiration"` // in second
 }
 
 func DefaultOAuth2Variable() OAuth2Variable {
 	return OAuth2Variable{
 		IdPLoginURL:                      "http://localhost:7063/login",
 		ClientSecretLength:               64,
-		AuthorizationCodeFlowExpiration:  10 * 60, // 10m
-		AuthenticationCallbackExpiration: 5 * 60,  // 5m
-		SessionUpdateExpiration:          15,      // 15s
+		AuthorizationCodeFlowExpiration:  10 * 60,           // 10m
+		AuthenticationCallbackExpiration: 15 * 60,           // 15m
+		SessionUpdateExpiration:          15,                // 15s
+		FailureConsentExpiration:         15,                // 15s
+		ConsentExpiration:                30 * 24 * 60 * 60, // 30d
 	}
 }
 
